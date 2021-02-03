69a0f72129578f54b3446fb5275612e260b1a65f
rafael-santiago <voidbrainvoid@tutanota.com>
2021-02-02 23:44:59 -0200

    Implement group evaluation mode

--- src/hysplex.h
+++ src/hysplex.h
@@ -35,7 +35,7 @@ struct hysplex_eval_group {
 
 #define HYSPLEX_REGISTER_FUNCTION(func) { #func, (hysplex_meta_func)func }
 
-#define HYSPLEX_FUNCTION_GROUP_END }
+#define HYSPLEX_FUNCTION_GROUP_END };
 
 #define HYSPLEX_FUNCTION_GROUP_SIZE(name) sizeof(name) / sizeof(name[0])
 
@@ -129,58 +129,87 @@ static FILE *hysplex_stdout = NULL;
 #define HYSPLEX_DO_EVAL_GROUP(iter_nr, warm_up, pre_run_stmt, post_run_stmt, group, group_nr, ...) {\
     ssize_t lg = 0;\
     for (ssize_t g = 1; g < group_nr; g++) {\
-        struct hysplex_stat hs[2] = { { NULL, 0, 0 }, { NULL, 0, 0 } };\
-        hs[0].func_name = group[lg].name;\
-        hs[1].func_name = group[g].name;\
-        if (warm_up) {\
-            HYSPLEX_INFO("cache warming up... wait...\n");\
+        struct hysplex_stat hs[2];\
+        ssize_t wi[2];\
+        char out[4096];\
+        for (size_t r = 0; r < 2; r++) {\
+            hysplex_meta_func f0, f1;\
+            if (r == 0) {\
+                hs[0].func_name = group[lg].name;\
+                hs[1].func_name = group[g].name;\
+                f0 = group[lg].func;\
+                f1 = group[g].func;\
+            } else {\
+                hs[0].func_name = group[g].name;\
+                hs[1].func_name = group[lg].name;\
+                f0 = group[g].func;\
+                f1 = group[lg].func;\
+            }\
+            hs[0].score = hs[1].score = 0;\
+            hs[0].exec_time_s = hs[1].exec_time_s = 0;\
+            if (warm_up) {\
+                HYSPLEX_INFO("cache warming up... wait...\n");\
+                for (size_t i = 0; i < iter_nr; i++) {\
+                    pre_run_stmt;\
+                    f0(__VA_ARGS__);\
+                    post_run_stmt;\
+                    pre_run_stmt;\
+                    f1(__VA_ARGS__);\
+                    post_run_stmt;\
+                    HYSPLEX_PROGRESS(i, iter_nr);\
+                }\
+            }\
+            fprintf(hysplex_stdout, "\r                                             \r"\
+                                    "hysplex info: done.\n\n");\
+            if (r == 0) {\
+                fprintf(hysplex_stdout, "hysplex info: '%s' vs. '%s'... wait...\n", group[lg].name, group[g].name);\
+            } else {\
+                fprintf(hysplex_stdout, "hysplex info: '%s' vs. '%s... wait...\n", group[g].name, group[lg].name);\
+            }\
             for (size_t i = 0; i < iter_nr; i++) {\
+                clock_t start, end;\
                 pre_run_stmt;\
-                group[lg].func(__VA_ARGS__);\
+                start = clock();\
+                f0(__VA_ARGS__);\
+                end = clock();\
                 post_run_stmt;\
+                long double f0_exec_time = ((long double)(end - start)) / ((long double)CLOCKS_PER_SEC);\
+                hs[0].exec_time_s += f0_exec_time;\
                 pre_run_stmt;\
-                group[g].func(__VA_ARGS__);\
+                start = clock();\
+                f1(__VA_ARGS__);\
+                end = clock();\
                 post_run_stmt;\
+                long double f1_exec_time = ((long double)(end - start)) / ((long double)CLOCKS_PER_SEC);\
+                hs[1].exec_time_s += f1_exec_time;\
+                if (f0_exec_time < f1_exec_time) {\
+                    hs[0].score += 1;\
+                } else if (f1_exec_time < f0_exec_time) {\
+                    hs[1].score += 1;\
+                } else {\
+                    hs[0].score += 1;\
+                    hs[1].score += 1;\
+                }\
                 HYSPLEX_PROGRESS(i, iter_nr);\
             }\
-        }\
-        fprintf(hysplex_stdout, "\r             \rdone.\n");\
-        fprintf(hysplex_stdout, "hysplex info: '%s' vs. '%s'... wait...\n", group[lg].name, group[g].name);\
-        for (size_t i = 0; i < iter_nr; i++) {\
-            clock_t begin, end;\
-            pre_run_stmt;\
-            start = clock();\
-            func0(__VA_ARGS__);\
-            end = clock();\
-            post_run_stmt;\
-            long double func0_exec_time = ((long double)(end - start)) / ((long double)CLOCKS_PER_SEC);\
-            hs[0].exec_time_s += func0_exec_time;\
-            pre_run_stmt;\
-            start = clock();\
-            func1(__VA_ARGS__);\
-            end = clock();\
-            post_run_stmt;\
-            long double func1_exec_time = ((long double)(end - start)) / ((long double)CLOCKS_PER_SEC);\
-            hs[1].exec_time_s += func1_exec_time;\
-            if (func0_exec_time < func1_exec_time) {\
-                hs[0].score += 1;\
-            } else if (func1_exec_time < func0_exec_time) {\
-                hs[1].score += 1;\
-            } else {\
-                hs[0].score += 1;\
-                hs[1].score += 1;\
+            fprintf(hysplex_stdout, "\r                                             \r"\
+                                "hysplex info: done.\n\n");\
+            if ((wi[r] = hysplex_get_winner_function((r == 0) ? out : NULL, (r == 0) ? sizeof(out) - 1 : 0, hs, 2, iter_nr, ((g + 1) == group_nr))) == -1) {\
+                HYSPLEX_WARN("The evaluated performances are statistically equal.\n"\
+                             "              In fact you must not taking into consideration performance to pick one.\n")\
+                exit(1);\
             }\
-            HYSPLEX_PROGRESS(i, iter_nr);\
         }\
-        fprintf(hysplex_stdout, "\r             \rdone.\n");\
-        if ((lg = hysplex_get_winner_function(hysplex_stdout, hs, 2, iter_nr, 1)) == -1) {\
-            HYSPLEX_WARN("The evaluated performances are statistically equal.\n"\
-                         "              In fact you must not taking into consideration performance to pick one.\n")\
+        if (wi[0] == wi[1]) {\
+            HYSPLEX_WARN("The evaluated performances are inconlusive. They are almost the same in performance. A bias was detected.\n"\
+                         "              The second function seems to take advantage from the first, probably due to CPU cache.\n"\
+                         "              You should take into consideration other aspects than performance to pick one. It is up to you!\n")\
             exit(1);\
         }\
-        if (lg == 1) {\
+        if (wi[0] == 1) {\
             lg = g;\
         }\
+        fprintf(hysplex_stdout, "%s\n", out);\
     }\
 }
 
@@ -213,8 +242,9 @@ HYSPLEX_MAIN_END
 
 #define HYSPLEX_EVAL_GROUP(iter_nr, warm_up, pre_run_stmt, post_run_stmt, group, ...)\
     HYSPLEX_MAIN_BEGIN\
-        HYSPLEX_DO_EVAL_GROUP(iter_nr, warm_up, pre_run_stmt, post_run_stmt, group, HYSPLEX_FUNCTION_GROUP_SIZE(group),\
-                              __VA_ARGS__)\
+        HYSPLEX_BANNER\
+        HYSPLEX_DO_EVAL_GROUP(iter_nr, warm_up, pre_run_stmt, post_run_stmt, group,\
+                              HYSPLEX_FUNCTION_GROUP_SIZE(group), __VA_ARGS__)\
 HYSPLEX_MAIN_END
 
 ssize_t hysplex_get_winner_function(char *buf, const size_t buf_size, struct hysplex_stat *hs, const size_t hs_nr,
---
+++ src/samples/eval_group.c
@@ -0,0 +1,43 @@
+#include <hysplex.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int stat_based_file_exists(const char *filepath) {
+    if (filepath == NULL) {
+        return 0;
+    }
+    struct stat st;
+    return (stat(filepath, &st) == 0);
+}
+
+int fopen_based_file_exists(const char *filepath) {
+    if (filepath == NULL) {
+        return 0;
+    }
+    FILE *fp;
+    int retval = ((fp = fopen(filepath, "r")) != NULL);
+    if (retval) {
+        fclose(fp);
+    }
+    return retval;
+}
+
+int open_based_file_exists(const char *filepath) {
+    if (filepath == NULL) {
+        return 0;
+    }
+    int fd;
+    int retval = ((fd = open(filepath, O_RDONLY)) != -1);
+    if (retval > -1) {
+        close(fd);
+    }
+    return retval;
+}
+
+HYSPLEX_FUNCTION_GROUP_BEGIN(func_group)
+    HYSPLEX_REGISTER_FUNCTION(open_based_file_exists),
+    HYSPLEX_REGISTER_FUNCTION(fopen_based_file_exists),
+    HYSPLEX_REGISTER_FUNCTION(stat_based_file_exists)
+HYSPLEX_FUNCTION_GROUP_END
+
+HYSPLEX_EVAL_GROUP(500000, 1, {}, {}, func_group, "eval_group.c");
